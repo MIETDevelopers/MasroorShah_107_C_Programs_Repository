97684ed0488658caaf1c3c8c43ea18b8aa5a91af
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-17 20:35:57 +0530

    Check Even Or Odd Number

---
+++ 7_EvenOdd.c
@@ -0,0 +1,19 @@
+//Masroor Shah
+//Program: Check whether an entered number is even or odd
+
+
+#include<stdio.h> 
+int main(){ 
+	int number; 
+	printf("Enter a number: "); 
+	scanf("%d", &number); 
+	if (number%2==0)  
+	{
+		printf("%d is even", number);
+	}
+	else 
+	{
+		printf("%d is odd", number);
+	}
+	return 0;
+}
