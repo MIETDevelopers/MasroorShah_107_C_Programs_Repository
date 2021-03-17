a44dc219887297d08d74f05e3b1880b8ab864fe4
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-17 22:39:45 +0530

    Area, Perimeter with user defined vallues

---
+++ 9_APerimeter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
+	raduisOfCircle=8;
+	sideOfSquare=10;
+	legthOfRectangle=7;
+	breadthOfRectangle=5;
+		printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
+		printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));
+		printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
+		printf("\nPerimeter of Square: %f",(4*sideOfSquare));
+		printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
+		printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));
+return 0;
+}
+
+
+
+
+
+
+
+
