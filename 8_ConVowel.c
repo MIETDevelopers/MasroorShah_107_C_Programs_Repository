02b1260af720167baf6edc97c5ac3bc5fe72b11e
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-18 14:22:05 +0530

    Vowel or Consonant with comments

--- 8_ConVowel.c
+++ 8_ConVowel.c
@@ -1,11 +1,13 @@
 //Masroor Shah
-//Program: Even or odd alphabets
-#include <stdio.h>
-int main(){
-   char ch;
-   printf("Enter character\n");
-   scanf("%c",&ch);
-      switch(ch){
+//Program: Check whether alphabet is vowel or consonant
+
+#include <stdio.h> //Pre-process directive to include standard input and output functions header f
+int main(){ //main function
+   char c; //variable declaration with data type char
+   printf("Enter character\n"); //printf function calling
+   scanf("%c",&c); //scanf function calling
+     //variable definition using switch case for both uppercase and lowercase variable "c"
+      switch(c){  
       case'a':
       case'e':
       case'i':
@@ -16,11 +18,12 @@ int main(){
       case'I':
       case'O':
       case'U':
-         printf("%c is a vowel",ch);
-      break;
+         printf("%c is a vowel",c); //printf function calling
+      break; 
+      //if variable doesn't match any case variable 
       default:
-         printf("%c is a consonant",ch);
+         printf("%c is a consonant",c);
             break;
 }  
-return 0;
+return 0; //return function
 }
