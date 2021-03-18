9f771f80e7cabfeac2201e14171506677ec5b8e5
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-18 13:34:05 +0530

    Arithmetic op with comments

--- 4_Calculator.c
+++ 4_Calculator.c
@@ -1,29 +1,33 @@
-  #include <stdio.h>
-int main() {
-  char operator;
-    double first, second;
-    printf("Enter an operator (+, -, *,): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf", &first, &second);
+//Name: Masroor Hussain Shah 
+//Program: Understanding different arithmetic operators using a simple calclator
 
-    switch (operator) {
-    case '+':
-        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+
+#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+int main() {   //main fucntion
+  char operator; //variable declaraion with data type char
+    double first, second; //variable declaration with datatype double 
+    printf("Enter an operator (+, -, *,): "); //printf funtion to print operator
+    scanf("%c", &operator);  //scanf function to read the opperator from the user
+    printf("Enter two operands: "); //printf fucnction to print operands
+    scanf("%lf %lf", &first, &second); //scanf function to read thetwo input operands
+
+    switch (operator) {    //declaration of switch case operator
+    case '+': 
+        printf("%.1lf + %.1lf = %.1lf", first, second, first + second); //printf function calling for addition 
         break;
     case '-':
-        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+        printf("%.1lf - %.1lf = %.1lf", first, second, first - second); //printf function callinf for subtraction
         break;
     case '*':
-        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+        printf("%.1lf * %.1lf = %.1lf", first, second, first * second); //printf function callinf for multiplication
         break;
     case '/':
-        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        printf("%.1lf / %.1lf = %.1lf", first, second, first / second); //printf function callinf for division
         break;
-        // operator doesn't match any case constant
+        // if operator doesn't match any case constant
     default:
         printf("Error! operator is not correct");
     }
 
-    return 0;
+    return 0; //return function
 }
