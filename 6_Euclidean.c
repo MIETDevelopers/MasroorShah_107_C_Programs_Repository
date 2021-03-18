01bff0a18fca5d3b2e5ad45ebad324ec54254c2c
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-18 13:52:45 +0530

    Euclidean distance program with comments

--- 6_Euclidean.c
+++ 6_Euclidean.c
@@ -1,18 +1,17 @@
-//Masroor Shah
-// Euclidean Program
+//Name:Masroor Shah
+// Program: Euclidean Program
 
 
 
-#include <stdio.h>  
-#include <math.h>
-
-  	int main(){
-		float x1,y1,x2,y2,distance;
-		printf("Enter point one(x1,y1)\n");
-		scanf("%f %f",&x1,&y1);
-		printf("Enter point two(x1,y1)\n");
-		scanf("%f %f",&x2,&y2);
-		distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-			printf("distance between(%f,%f)&(%f,%f)is %f\n",x1,y1,x2,y1,distance);
-	return 0;
+#include <stdio.h>  //Pre-process directive to include standard input and output functions header file
+#include <math.h> //Pre-process directive to include mathematical functions header file
+	int main(){ //Main function
+		float x1,y1,x2,y2,distance; //variable declaration with data type float
+		printf("Enter point one(x1,y1)\n"); //printf function calling
+		scanf("%f %f",&x1,&y1); //scanf function for taking x1, y1 cordinate
+		printf("Enter point two(x1,y1)\n"); //printf function calling
+		scanf("%f %f",&x2,&y2); //scanf function for taking x2, y2 cordinate
+		distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)); //variable defintion for Euclidean distance
+			printf("distance between(%f,%f)&(%f,%f)is %f\n",x1,y1,x2,y1,distance);  //printf function calling
+	return 0; //return function
 }
