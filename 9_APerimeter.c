81ebe441e4332f0d15c74fa0eb754746e5e93628
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-18 21:59:49 +0530

    Comments added to Area Perimeter program

--- 9_APerimeter.c
+++ 9_APerimeter.c
@@ -1,17 +1,24 @@
-#include <stdio.h>
-int main(){
-	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
-	raduisOfCircle=8;
-	sideOfSquare=10;
+/*
+Name: Masroor Hussain Shah
+Program Purpose: To find out area of Circle, Rectangle and Square based on user choice
+*/
+
+#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+int main(){ //Main function
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle; //variable declaration using float datatype
+// variable value declaration	
+	raduisOfCircle=8; 
+	sideOfSquare=10;  
 	legthOfRectangle=7;
 	breadthOfRectangle=5;
+			//printf function calling
 		printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
 		printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));
 		printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
 		printf("\nPerimeter of Square: %f",(4*sideOfSquare));
 		printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
 		printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));
-return 0;
+return 0;  //return function
 }
 
 
