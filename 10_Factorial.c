c72ba7dad07dedc0c57fd336d92d444a9cba4b06
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-19 01:07:49 +0530

    Factorial using forloop (with comments)

--- 10_Factorial.c
+++ 10_Factorial.c
@@ -1,18 +1,18 @@
-//Masroor Shah	
-//Factorial Using ForLoop 
+//Name: Masroor Hussain Shah	
+//Program: Factorial Using ForLoop 
 
 
-#include<stdio.h>
-int main(){
- 	int i,f=1,num;
- 	printf("Enter an integer");
-  	scanf("%d",&num);
- 
-  		for(i=1;i<=num;i++)
+#include<stdio.h> ////Pre-process directive to include standard input and output functions header file
+int main(){ //main function
+ 	int i,f=1,num; //interger variable's value declaration 
+ 	printf("Enter an integer"); //printf function calling
+  	scanf("%d",&num); //scanf function calling
+ //for loop from 1 upto num
+  		for(i=1;i<=num;i++) 
       		f=f*i;
  
-  		printf("Factorial of %d is: %d",num,f);
-  return 0;
+  		printf("Factorial of %d is: %d",num,f); //printf function calling
+  return 0; //return functio 
 }
 
 
