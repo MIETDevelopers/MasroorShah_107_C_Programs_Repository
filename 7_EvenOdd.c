9a4be25cb0f013916e8653b295e13263def29c49
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-18 14:02:35 +0530

    Even or odd program with comments

--- 7_EvenOdd.c
+++ 7_EvenOdd.c
@@ -1,19 +1,16 @@
 //Masroor Shah
 //Program: Check whether an entered number is even or odd
 
-
-#include<stdio.h> 
-int main(){ 
-	int number; 
-	printf("Enter a number: "); 
-	scanf("%d", &number); 
-	if (number%2==0)  
-	{
-		printf("%d is even", number);
+#include<stdio.h> //Pre-process directive to include standard input and output functions header file
+int main(){  //main function
+	int number;  //variable delaration with data type int
+	printf("Enter a number: "); //printf function calling
+	scanf("%d", &number); //scanf function calling to receive input
+	if (number%2==0){ //declaration of if else statement
+		printf("%d is even", number); //if  the remainder of a number is 0 after dividing with two, print even
 	}
-	else 
-	{
-		printf("%d is odd", number);
+	else{
+		printf("%d is odd", number); //if  the remainder of a number is not 0 after dividing with two, print dd
 	}
-	return 0;
+	return 0; //return function
 }
