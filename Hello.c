eeab5e5670633ff1b4e03bcd1ddfa5c51b6d9497
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-22 18:07:56 +0530

    Hello world assignment Q4

---
+++ Hello.c
@@ -0,0 +1,9 @@
+//Name: Masroor Hussain Shah
+//Program:Hello World
+
+#include <stdio.h> ////Pre-process directive to include standard input and output functions header file
+
+int main(){      //Main Function
+  printf("Hello World\n");   //Printf function calling (to print hello world)
+  return 0; //return function
+}
