de9f7c169ea8aa9d116cc0ddfff0280ef723390b
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-22 18:04:53 +0530

    Euclidean Q2 assignment

---
+++ Euclidean.c
@@ -0,0 +1,17 @@
+//Name:Masroor Shah
+// Program: Euclidean Program
+
+
+
+#include <stdio.h>  //Pre-process directive to include standard input and output functions header file
+#include <math.h> //Pre-process directive to include mathematical functions header file
+	int main(){ //Main function
+		float x1,y1,x2,y2,distance; //variable declaration with data type float
+		printf("Enter point one(x1,y1)\n"); //printf function calling
+		scanf("%f %f",&x1,&y1); //scanf function for taking x1, y1 cordinate
+		printf("Enter point two(x2,y2)\n"); //printf function calling
+		scanf("%f %f",&x2,&y2); //scanf function for taking x2, y2 cordinate
+		distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)); //variable defintion for Euclidean distance
+			printf("distance between(%f,%f)&(%f,%f)is %f\n",x1,y1,x2,y1,distance);  //printf function calling
+	return 0; //return function
+}
