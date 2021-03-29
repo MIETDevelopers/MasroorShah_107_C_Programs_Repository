1daf04f3f454ec487977789143cc83ef79bf247a
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-03-29 12:28:50 +0530

    Use of string Operations

--- 21_StringOperation.c
+++ 21_StringOperation.c
@@ -1,19 +1,12 @@
 //Name: Masroor Hussain Shah
-//Program: Read the string in pointers loaction and give it as output.			
+//Program: To demonstrate the use of string operations
 
-#include <stdio.h>		//Pre-process directive to include standard input and output functions header file
-
-int main(void) {
-  char name[] = "Mr Bean";
-
-  printf("%c \n", *name);     	// Output will be I
-  printf("%c \n", *(name+3));	// Output will be A
-  printf("%c \n", *(name+8));   // Output will be R
-
-  char *namePtr;
-
-  namePtr = name;
-  printf("%c \n", *namePtr);     // Output will be I
-  printf("%c \n", *(namePtr+2)); // Output will be H
-  printf("%c \n", *(namePtr+7)); // Output will be E
+#include<stdio.h> //Pre-processor directive to include standard input and output functions header file
+int main(void){
+	char name[30];
+    	printf("Enter name: ");
+	gets(name);     //Function to read string from user
+	printf("Name: ");
+	puts(name);    //Function to display string
+	return 0;    //Return function
 }
