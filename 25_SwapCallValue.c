5b1f1e136df80ddee573613e99461fef55e988cc
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-04-12 19:02:41 +0530

    Call by Value

--- 25_SwapCallValue.c
+++ 25_SwapCallValue.c
@@ -1,19 +1,19 @@
  //Name: Masroor Hussain Shah 
 //Program: Swap two numbers using call by value. ()        
-#include <stdio.h> //Pre-process directive to include standard input and output functions header file
-  
-int main() {        //main function
-    int x, y; 
-    printf("Enter Value of x "); 
-    scanf("%d", &x); 
-    printf("\nEnter Value of y "); 
-    scanf("%d", &y); 
-    
-    //Here swapping of number occurs.
-    int temp = x; 
-    x = y; 
-    y = temp; 
-  
-    printf("\nAfter Swapping: x = %d, y = %d", x, y); 
-    return 0;   //return function
+ 
+#include<stdio.h>  //Pre-process directive to include standard input and output functions header file
+void swap(int, int);
+int main(){
+    int a, b;
+    printf("Enter two values: ");
+    scanf("%d %d", &a, &b);//Taking input from user.
+    swap(a,b);
+}
+//Here swapping exists.
+void swap (int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("%d %d", a, b);
 }
